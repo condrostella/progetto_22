3feb251a608bab4e2f9ab33d10517b44d43720fa
Donia-F <doniaferjani20@gmail.com>
2022-09-28 11:58:18 +0200

    init disk_driver.c

---
+++ disk_driver.c
@@ -0,0 +1,53 @@
+#include "disk_driver.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>  
+#include <sys/types.h>  
+#include <sys/mman.h> 
+#include <errno.h>
+#include <unistd.h> 
+#include <fcntl.h>  
+#include <ctype.h> 
+#include <string.h>  
+#include <errno.h>  
+#include <signal.h> 
+
+
+
+
+void DiskDriver_init(DiskDriver* disk, const char* filename, int num_blocks){
+
+	int file;
+	file = open (filename, O_CREAT |O_RDWR | O_SYNC,0666);
+	if (file == -1) {
+		perror("errore nell'apertura del file");
+	} 
+	
+	//dopo aver controllato che la open è andata a buon fine inizializzo il file descriptor del disco con il file descriptor del file appena aperto
+	disk->fd = file;
+	//impostiamo la dimensione del disco
+	int ret=ftruncate(disk->fd,sizeof(DiskHeader)+num_blocks*sizeof(int)+num_blocks*BLOCK_SIZE);
+	if(ret==-1){
+	    perror("errore ftruncate");
+	}
+
+	//mappiamo l'header in memoria e inizializziamo i campi del DiskHeader
+	disk->header = mmap (0, sizeof(DiskHeader), PROT_READ | PROT_WRITE, MAP_SHARED, disk->fd, 0); 
+	if(disk->header==MAP_FAILED){
+	    perror("errore nella mmap dell'header \n");
+	}
+	disk->header->num_blocks=num_blocks;
+	disk->header->free_blocks=num_blocks;
+	disk->header->first_free_block=0;
+	printf("%d\n",disk->header->free_blocks);
+
+	//mappiamo la fat in memoria e inizializziamo i blocchi della fat a -1
+	disk->fat = mmap(0,num_blocks*sizeof(int),PROT_READ | PROT_WRITE,MAP_SHARED,disk->fd,sizeof(DiskHeader));
+	if(disk->fat==MAP_FAILED){
+	    perror("errore nella mmap della fat \n");
+	}
+	for(int i=0;i<num_blocks;i++){
+	    disk->fat[i]=-1;
+    }
+   
+}
